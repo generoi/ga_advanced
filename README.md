d242c9a761295b59bdbd3062615a8fb613b3807c
Oskar Schöldström <public@oxy.fi>
2015-08-16 13:47:53 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+GA Events
+=========
+
+Drupal module
+
+### Done
+
+- Colorbox dialogs
+- User timings: Page scroll
+
+### TODO
+
+- More social events
+- Support async loaded social libraries
+- Advanced dimensions/metrics (eg. sales profit)
+- Tabs (probably configurable)
+- Drupal: Vartiation selection
+- Drupal: Quanity change
+- Drupal: User registration (this is porbably not an event)
+- Drupal: Write review (this is probably not an event)
+- Drupal: Sign in (this is probably not an event)
+- User timings: Javascript execution
+- User timings: Drupal execution
+- Commerce: Impressions on all listings
+- Commerce: Product click
+- Commerce: Product detail view
+- Commerce: Add/remove from cart
+- Commerce: Checkout process
+- Commerce: Refunds, transactions
+- Commerce: Promotions
+
+### Supported in Google Analytics
+
+- Tracking of Drupal and JS errors
+- Enhanced link attribution (@todo add more ids?)
+- Simple dimensions and metrics
+- Downloads
+- External links
+- Colorbox pageviews
---
+++ js/ga_events.js
@@ -0,0 +1,57 @@
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.gaEvents = {
+    attach: function (context) {
+      if (this.processed) return;
+      this.processed = true;
+
+      // Track scroll depth.
+      $.scrollDepth();
+
+      // Track facebook events
+      if (FB && FB.Event) Drupal.gaEvents.attachFacebook();
+
+      // Track colorbox links
+      $(document)
+        .on('click', 'a.colorbox-iframe', function () {
+          event('Modla dialog', 'open', Drupal.googleanalytics.getPageUrl(this.href));
+        })
+        // Fivestar
+        .on('click', '.fivestar-widget a', function () {
+          event('Rate', $(this).text());
+        });
+    }
+  };
+
+  Drupal.gaEvents = Drupal.ga || {};
+
+  Drupal.gaEvents.ga = function (type, args) {
+    if (typeof ga !== 'undefined') window.ga.apply(window, ['send', type].concat(args));
+    if (typeof _gaq !== 'undefined') window._gaq.push(['_track' + type.charAt(0).toUpperCase() + type.slice(1)].concat(args));
+    if (window.DEBUG) console.log('track ' + type, args);
+  };
+
+  Drupal.gaEvents.event = function event() {
+    var args = Array.prototype.slice.call(arguments);
+    Drupal.gaEvents.ga('event', args);
+  };
+
+  Drupal.gaEvents.attachFacebook = function attachFacebook() {
+    FB.Event.subscribe('edge.create', function (url) {
+      Drupal.gaEvents.ga('social', 'facebook', 'like', Drupal.googleanalytics.getPageUrl(url));
+    });
+    FB.Event.subscribe('edge.remove', function (url) {
+      Drupal.gaEvents.ga('social', 'facebook', 'unlike', Drupal.googleanalytics.getPageUrl(url));
+    });
+    FB.Event.subscribe('comment.create', function () {
+      Drupal.gaEvents.ga('social', 'facebook', 'comment', Drupal.googleanalytics.getPageUrl(window.location.href));
+    });
+    FB.Event.subscribe('comment.delete', function () {
+      Drupal.gaEvents.ga('social', 'facebook', 'uncomment', Drupal.googleanalytics.getPageUrl(window.location.href));
+    });
+    FB.Event.subscribe('message.send', function (url) {
+      Drupal.gaEvents.ga('social', 'facebook', 'message', Drupal.googleanalytics.getPageUrl(url));
+    });
+  };
+}(jQuery));
